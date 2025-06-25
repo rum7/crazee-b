9be7223c940993942fe102172b42dd8ec4e0e8b1
rum7 <87144903+rum7@users.noreply.github.com>
2025-06-25 19:51:11 +0200

    ♻️ F01: App.tsx Create LoginPage with the LoginForm component

---
+++ src/LoginPage.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from "./components/form-login";
+
+export function LoginPage() {
+    // state (état, données)
+
+    // comportements
+
+    // affichage (render)
+    return (
+        <LoginForm />
+    )    
+}
