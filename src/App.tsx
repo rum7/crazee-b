5f10ba980d27873497d38bfefe739d2c061c749e
rum7 <87144903+rum7@users.noreply.github.com>
2025-06-25 19:39:51 +0200

    ✨ F01: App.tsx Create titles and form login

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,22 @@
 export default function App() {
+    // state (état, données)
+    
+    // comportements
+
+    // affichage (render)
+    return (
+        <div>
+            <h1>Bienvenue chez nous !</h1>
+            <h2>Connectez-vous</h2>
+            <form>
+                <input 
+                    type="text" 
+                    name="prenom" 
+                    placeholder="Entrez votre prénom..." 
+                    required 
+                />
+                <button type="submit">Accédez à votre espace</button>
+            </form>
+        </div>
+    )    
 }
