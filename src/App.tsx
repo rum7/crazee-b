a37001b858948831cbf85bab8e32165ac12ffa0c
rum7 <87144903+rum7@users.noreply.github.com>
2025-06-25 19:45:23 +0200

    ✨ F01: App.tsx Handle the form with the alert

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ export default function App() {
     // state (état, données)
     
     // comportements
+    const handleLogin = (e: React.FormEvent) => {
+        e.preventDefault()
+
+        const formData: FormData = new FormData(e.target as HTMLFormElement)
+        const prenom = formData.get('prenom')
+        alert(`Bonjour ${prenom}`)
+
+    }
 
     // affichage (render)
     return (
         <div>
             <h1>Bienvenue chez nous !</h1>
             <h2>Connectez-vous</h2>
-            <form>
+            <form
+                onSubmit={handleLogin}
+            >
                 <input 
                     type="text" 
                     name="prenom" 
