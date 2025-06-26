f7e5c47053b0157488191821fe890794357013c8
rum7 <87144903+rum7@users.noreply.github.com>
2025-06-26 18:04:15 +0200

    F02: ERROR > Create the error page

---
+++ src/components/pages/error/ErrorPage.tsx
@@ -0,0 +1,8 @@
+export function ErrorPage() {
+  return (
+    <div>
+        <h1>ErrorPage</h1>
+        <a href="/">Retourner à la page d'accueil</a>
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
+
 import App from './App.tsx'
+import { ErrorPage } from './components/pages/error/ErrorPage.tsx'
+
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
@@ -9,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<App />} />
+                <Route path="*" element={<ErrorPage />} />
             </Routes>
         </BrowserRouter>
     </StrictMode>,
