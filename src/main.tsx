2a7a4084e179604f24f96d30c2ca0ec22826cefc
rum7 <87144903+rum7@users.noreply.github.com>
2025-06-27 22:31:10 +0200

    💄 F03: RESET > Reset some css properties

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router'
 
 import App from './App.tsx'
 import './index.css'
+import './reset.css'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
