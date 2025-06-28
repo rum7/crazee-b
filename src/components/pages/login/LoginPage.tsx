68077db64321d7f6397050eb38627b711686b311
rum7 <87144903+rum7@users.noreply.github.com>
2025-06-28 21:35:47 +0200

    💄 F03: LOGIN > Adjust layout and add style to logo

--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -1,4 +1,5 @@
 import { LoginForm } from "./LoginForm";
+import logo from "../../../assets/logo-crazee-burger.png"
 
 export function LoginPage() {
     // state (état, données)
@@ -7,6 +8,13 @@ export function LoginPage() {
 
     // affichage (render)
     return (
-        <LoginForm />
+        <main className="auth">
+            <div className="auth_logo">
+                <span>Crazee</span>
+                <img src={logo} alt="logo" />
+                <span>Burger</span>
+            </div>
+            <LoginForm />
+        </main>
     )    
 }
