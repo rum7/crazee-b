47db21531ad10fca309bde95c1fc2a1c03b9bc4d
rum7 <87144903+rum7@users.noreply.github.com>
2025-07-01 16:03:54 +0200

    ♻️ F03: LOGINFORM > INPUT > Create the Input component

--- src/components/pages/login/LoginForm.tsx
+++ src/components/pages/login/LoginForm.tsx
@@ -1,46 +1,48 @@
-import { useRef } from "react"
-import { FaChevronRight } from "react-icons/fa"
-import { IoPersonCircleOutline } from "react-icons/io5"
-import { useNavigate } from "react-router"
+import { useRef } from 'react'
+import { FaChevronRight } from 'react-icons/fa'
+import { useNavigate } from 'react-router'
+import { Input } from '../../ui/input/Input'
+import { IoPersonCircleOutline } from 'react-icons/io5'
 
 export function LoginForm() {
-    // state (état, données)
-    const formRef = useRef<HTMLFormElement>(null)
-    const navigate = useNavigate()
+	// state (état, données)
+	const formRef = useRef<HTMLFormElement>(null)
+	const navigate = useNavigate()
 
-    // comportements
-    const handleLogin = (e: React.FormEvent) => {
-        e.preventDefault()
+	// comportements
+	const handleLogin = (e: React.FormEvent) => {
+		e.preventDefault()
 
-        const formData: FormData = new FormData(e.target as HTMLFormElement)
-        const prenom = formData.get('prenom')
-        formRef.current?.reset()
+		const formData: FormData = new FormData(e.target as HTMLFormElement)
+		const prenom = formData.get('prenom')
+		formRef.current?.reset()
 
-        navigate(`/order/${prenom}`)
-    }
+		navigate(`/order/${prenom}`)
+	}
 
-    // affichage (render)
-    return (
-        <form 
-            ref={formRef}
-            onSubmit={handleLogin}
-            className="auth_form"
+	// affichage (render)
+	return (
+		<form
+			ref={formRef}
+			onSubmit={handleLogin}
+			className="auth_form"
         >
-            <h1>Bienvenue chez nous !</h1>
-            
-            <div className="auth_container">
-                <h2>Connectez-vous</h2>
-                <div className="auth_input_wrapper">
-                    <IoPersonCircleOutline color="#747B91" size={24} />
-                    <input
-                        type="text"
-                        name="prenom"
-                        placeholder="Entrez votre prénom"
-                        required
-                    />
-                </div>
-                <button type="submit">Accéder à mon espace<FaChevronRight /></button>
-            </div>
-        </form>
-    )    
+			<h1>Bienvenue chez nous !</h1>
+
+			<div className="auth_container">
+				<h2>Connectez-vous</h2>
+				<Input
+					name="prenom"
+					placeholder="Entrez votre prénom"
+					required
+                    Icon={<IoPersonCircleOutline className="input_icon" />}
+				/>
+
+				<button type="submit">
+					Accéder à mon espace
+					<FaChevronRight />
+				</button>
+			</div>
+		</form>
+	)
 }
